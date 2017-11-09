f532e38a87dad1d9980970ac79d31a415f31f730
4onen <mdupree@umail.ucsb.edu>
2017-11-09 14:23:55 -0800

    Completed expression class skeleton.

--- expression.h
+++ expression.h
@@ -1,4 +1,17 @@
 #ifndef EXPRESSION_H
 #define EXPRESSION_H
 
+#include "list.h"
+
+class expression{
+    public:
+        expression(string pInfix);
+        expression(const expression &source);
+        string infixString() const;
+        string prefixString() const;
+        string postfixString() const;
+    private:
+        Node* tree;
+}
+
 #endif //EXPRESSION_H
